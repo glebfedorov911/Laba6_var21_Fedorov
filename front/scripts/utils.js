9323e1d0e02d83318a92d6d671599e011a28f553
Gleb <fedorov22134@gmail.com>
2025-05-05 04:29:40 +0300

    add some commetns

--- front/scripts/utils.js
+++ front/scripts/utils.js
@@ -11,17 +11,36 @@ export function redirectUserIfAuth() {
     }
 }
 
+/**
+ * Очищает контейнеры после того как они были закрыты
+ *
+ * @param string id айди элемента
+ * return этот контейнер
+ */
 export function clearErrorContainer(id) {
     let container = document.getElementById(id);
     container.innerHTML = "";
     return container;
 }
 
+/**
+ * Отправляет пост запрос на бэк
+ *
+ * @param string url путь до файла
+ * @param object data данные для отправки
+ * return ответ сервера
+ */
 export function sendDataToBack(url, data) {
     let client = new HTTPClient()
     return client.request("POST", {"apiUrl": url, "userData": data})
 }
 
+/**
+ * Заполняет контейнер с ошибками которые вернул бэк
+ *
+ * @param HTTPElementDoc element элемент
+ * @param object errors ошибки со стороны бэка
+ */
 export function fillContainerWithError(element, errors) {
     let ol = document.createElement("ol");
     for (let error in errors) {
@@ -34,11 +53,22 @@ export function fillContainerWithError(element, errors) {
     element.style.display = "block";
 }
 
+/**
+ * Cохраняет данные об авторизованном пользователе в локалсторадж
+ *
+ * @param object data данные о юзере
+ */
 export function authUserLocalStorage(data) {
     data["data"]["password"] = ""
     localStorage.setItem("userInfo", JSON.stringify(data["data"]))
 }
 
+/**
+ * Отправляет гет запрос на бэк
+ *
+ * @param string url путь до файла
+ * return ответ сервера
+ */
 export function getFromPhp(url) {
     let client = new HTTPClient()
     return client.request("GET", {"apiUrl": url})
